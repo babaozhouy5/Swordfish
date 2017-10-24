befd2c6bca77db397ae0cf3696fd849140562b78
babaozhouy5 <1609032563@qq.com>
2017-10-24 11:27:33 +0800

    modify wx&tuning token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "helloworld";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "b1a94d1906d64218b442f91bacd30192"; 
 	
 }
